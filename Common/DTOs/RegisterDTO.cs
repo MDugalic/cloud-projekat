6aff2eaed7f0e485d683ff41897489c6fc5ddbcc
MDugalic <milandugalici5@gmail.com>
2025-08-15 16:40:48 +0200

    Set default route to Account/Login and improve Register UI/validation

--- Common/DTOs/RegisterDTO.cs
+++ Common/DTOs/RegisterDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,31 @@ namespace Common.DTOs
 {
     public class RegisterDTO
     {
+        [Required, StringLength(80)]
         public string FullName { get; set; }
-        public string Gender { get; set; }    // optional: "M","F","Other"
+
+        [Required]                           // biće biran iz dropdown-a
+        public string Gender { get; set; }   // "Male", "Female", "Other"
+
+        [Required, StringLength(60)]
         public string Country { get; set; }
+
+        [Required, StringLength(60)]
         public string City { get; set; }
+
+        [Required, StringLength(120)]
         public string Address { get; set; }
 
+        [Required, EmailAddress]
         public string Email { get; set; }
+
+        [Required, DataType(DataType.Password), MinLength(6)]
         public string Password { get; set; }
 
-        // URL do slike (upload ćemo dodati kasnije; za početak može plain URL)
-        public string PhotoUrl { get; set; }
+        [Required, DataType(DataType.Password), Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Url(ErrorMessage = "Must be a valid URL")]
+        public string PhotoUrl { get; set; } // može i [Required] ako želiš da bude obavezna
     }
 }
